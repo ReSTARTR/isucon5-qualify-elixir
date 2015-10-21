e0cfd9b9f31194c89b6075d55ebf2d418d0c2e12
Masaki Yoshida <yoshida.masaki+restartr@gmail.com>
2015-10-21 18:21:10 +0900

    Add bootstrap.min.css to export files

--- brunch-config.js
+++ brunch-config.js
@@ -21,7 +21,8 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: "css/app.css",
+      joinTo: "css/bootstrap.min.css"
     },
     templates: {
       joinTo: "js/app.js"
